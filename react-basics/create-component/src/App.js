aff961bef7bb80cac6da02e7a0a64f1898e4fcd9
Meenu <meenucj1796@email.com>
2025-12-29 18:42:02 +0000

    This is your Lab-1 Your First Component

--- react-basics/create-component/src/App.js
+++ react-basics/create-component/src/App.js
@@ -1,14 +1,16 @@
-//to get output, run command - npm start
-
-import logo from './logo.svg';
-import './App.css';
-
-function Header() {
-  return <h1>Hello World!</h1>
-}
-function App() {
-  return <Header/>
-}
-
+function Heading() { 
+  return ( 
+    <h1>This is an h1 heading.</h1> 
+  ) 
+} 
+ 
+function App() { 
+  return ( 
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Heading /> 
+    </div> 
+  ); 
+} 
+ 
 export default App;
-
