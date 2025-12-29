3220c018ae3af2b560e1da350829b7c8fa364db7
Meenu <meenucj1796@email.com>
2025-12-29 18:00:01 +0000

    Fixed unclickable folder and uploaded React files

---
+++ react-basics/create-component/src/App.js
@@ -0,0 +1,14 @@
+//to get output, run command - npm start
+
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return <h1>Hello World!</h1>
+}
+function App() {
+  return <Header/>
+}
+
+export default App;
+
