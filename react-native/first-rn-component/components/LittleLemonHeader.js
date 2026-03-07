7a86f0c1d16b167a505f49cb5e896ac62ad92654
Meenu <meenucj1796@email.com>
2026-03-07 13:37:16 +0530

    FlatList, SectionList

--- react-native/first-rn-component/components/LittleLemonHeader.js
+++ react-native/first-rn-component/components/LittleLemonHeader.js
@@ -3,26 +3,20 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function LittleLemonHeader() {
   return (
-    <View style={headerStyles.container}>
-      <Text style={headerStyles.headerText}>
-	  Welcome to
-        <Text style={headerStyles.innerText}> Little Lemon</Text>
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Little Lemon</Text>
     </View>
   );
 }
 
-const headerStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#F4CE14',
+    backgroundColor: '#EE9972',
   },
   headerText: {
     padding: 40,
     fontSize: 30,
     color: 'black',
     textAlign: 'center',
-  },
-  innerText: {
-    fontWeight: 'bold',
   },
 });
--- react-native/first-rn-component/components/MenuItems.js
+++ react-native/first-rn-component/components/MenuItems.js
@@ -1,43 +1,82 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const menuItemsToDisplay = [
-  'Hummus \nMoutabal \nFalafel \nMarinated Olives \nKofta \nEggplant Salad \nLentil Burger \nSmoked Salmon \nKofta Burger \nTurkish Kebab \nFries \nButtered Rice \nBread Sticks \nPita Pocket \nLentil Soup \nGreek Salad \nRice Pilaf \nBaklava \nTartufo \nTiramisu \nPanna Cotta',
+  { name: 'Hummus', price: '$5.00', id: '1A' },
+  { name: 'Moutabal', price: '$5.00', id: '2B' },
+  { name: 'Falafel', price: '$7.50', id: '3C' },
+  { name: 'Marinated Olives', price: '$5.00', id: '4D' },
+  { name: 'Kofta', price: '$5.00', id: '5E' },
+  { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
+  { name: 'Lentil Burger', price: '$10.00', id: '7G' },
+  { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
+  { name: 'Kofta Burger', price: '$11.00', id: '9I' },
+  { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
+  { name: 'Fries', price: '$3.00', id: '11K' },
+  { name: 'Buttered Rice', price: '$3.00', id: '12L' },
+  { name: 'Bread Sticks', price: '$3.00', id: '13M' },
+  { name: 'Pita Pocket', price: '$3.00', id: '14N' },
+  { name: 'Lentil Soup', price: '$3.75', id: '15O' },
+  { name: 'Greek Salad', price: '$6.00', id: '16Q' },
+  { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
+  { name: 'Baklava', price: '$3.00', id: '18S' },
+  { name: 'Tartufo', price: '$3.00', id: '19T' },
+  { name: 'Tiramisu', price: '$5.00', id: '20U' },
+  { name: 'Panna Cotta', price: '$5.00', id: '21V' },
 ];
 
+const Item = ({ name, price }) => (
+  <View style={menuStyles.innerContainer}>
+    <Text style={menuStyles.itemText}>{name}</Text>
+    <Text style={menuStyles.itemText}>{price}</Text>
+  </View>
+);
+
+const Separator = ()=> <View style={menuStyles.separator} />
+const Header = () => <Text style={menuStyles.headerText}>View Menu</Text>
+const Footer = () => <Text style={menuStyles.footerText}>All rights reserved by Little Lemon, 2022</Text>
+
+
 const MenuItems = () => {
+  const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
+
   return (
     <View style={menuStyles.container}>
-      <ScrollView
-        horizontal={false}
-        indicatorStyle={'white'}
-        style={menuStyles.innerContainer}>
-        <Text style={menuStyles.headerText}>View Menu</Text>
-        <Text style={menuStyles.itemText}>{menuItemsToDisplay[0]}</Text>
-      </ScrollView>
+      <FlatList
+        data={menuItemsToDisplay}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        ItemSeparatorComponent={Separator}
+        ListHeaderComponent={Header}
+        ListFooterComponent={Footer}></FlatList>
     </View>
   );
 };
 
 const menuStyles = StyleSheet.create({
   container: {
-    flex: 0.75,
+    flex: 1,
   },
   innerContainer: {
     paddingHorizontal: 40,
-    paddingVertical: 40,
-    backgroundColor: 'black',
-  },
-  headerText: {
-    color: 'white',
-    fontSize: 40,
-    flexWrap: 'wrap',
+    paddingVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   itemText: {
     color: '#F4CE14',
-    fontSize: 36,
+    fontSize: 20,
+  },
+  separator:{
+    borderBottomWidth:1,
+    borderColor:'#EDEFEE'
+  },
+  footerText:{
+    color:'#EDEFEE',
+    fontSize:20,
+    flexWrap:'wrap'
   },
 });
 
 export default MenuItems;
-
